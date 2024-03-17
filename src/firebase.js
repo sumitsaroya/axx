0f47c046fa6ea824788cb48d6d67756252cd4e5b
sumitsaroya <137597094+sumitsaroya@users.noreply.github.com>
2024-03-17 12:26:48 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,15 +4,16 @@ import 'firebase/auth';
 import 'firebase/storage';
 import 'firebase/functions';
 
-const config = {
-  apiKey: process.env.REACT_APP_FIRE_BASE_KEY,
-  authDomain: process.env.REACT_APP_FIRE_BASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIRE_BASE_DB_URL,
-  projectId: process.env.REACT_APP_FIRE_BASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIRE_BASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIRE_BASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIRE_BASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIRE_BASE_MEASURMENT_ID,
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDkieZcIMRukbAO5B8pvf3rpmgqt7YGMJ0",
+  authDomain: "webcam-7d18e.firebaseapp.com",
+  databaseURL: "https://webcam-7d18e-default-rtdb.firebaseio.com",
+  projectId: "webcam-7d18e",
+  storageBucket: "webcam-7d18e.appspot.com",
+  messagingSenderId: "357663373695",
+  appId: "1:357663373695:web:93c561bc8c46cdf2ca824a",
+  measurementId: "G-7D6XPEMD2X"
 };
 
 firebase.initializeApp(config);
